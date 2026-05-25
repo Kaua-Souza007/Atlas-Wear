aebda2916839a0a8515796c8eaa24e1e3e0034a7
kazuyasamuel7-glitch <kazuyasamuel7@gmail.com>
2026-05-25 11:15:33 -0300

    Add files via upload

--- C - System/main.c
+++ C - System/main.c
@@ -1,21 +1,37 @@
-#include "tela_carregamento.h"
-#include "menu.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 int main() {
 
-    loading(); // chama a tela de carregamento
+    int opcao;
 
-    // depois disso, seu sistema começa
-    printf("\n                         Bem-vindo ao sistema!\n");
-    sleep(2);
-    system("clear");
-    
-    menu();
-    
-    
+    do {
+
+        printf("\n=== SISTEMA DE VENDAS ===\n");
+        printf("1 - Adicionar Produto\n");
+        printf("2 - Finalizar Venda\n");
+        printf("0 - Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+
+            case 1:
+                printf("Produto adicionado!\n");
+                break;
+
+            case 2:
+                printf("Venda finalizada!\n");
+                break;
+
+            case 0:
+                printf("Saindo...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+        }
+
+    } while(opcao != 0);
 
     return 0;
 }
